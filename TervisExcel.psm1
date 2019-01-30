3190e6d6d13320110d3f4b2eeb99ea7f7a7c0670
Hamilton Perez <hambone124@users.noreply.github.com>
2019-01-30 09:21:32 -0500

    Fix Excel.Application object reference in functions

--- TervisExcel.psm1
+++ TervisExcel.psm1
@@ -1,12 +1,12 @@
 function Export-TervisExcelCustomerReports {
     param (
-        [Parameter(Mandatory)]$Workbook,
+        [Parameter(Mandatory)][ref]$Workbook,
         [Parameter(Mandatory)]$ExportDirectory
     )
     
-    $Date = Get-Date -Format "yyyy-MM-dd"
+    $Date = Get-Date -Format "yyyy-MM-dd HH.mm"
     $WorksheetNamesToExport = "By Channel", "ITC (By Sales Rep)", "By Channel (Qtrly)", "TOP SUMMARY SHEET"
-    $Workbook.Sheets | Where-Object "Name" -In $WorksheetNamesToExport | ForEach-Object {
+    $Workbook.Value.Sheets | Where-Object "Name" -In $WorksheetNamesToExport | ForEach-Object {
         $ExportPath = Join-Path -Path $ExportDirectory -ChildPath "$Date $($_.Name).pdf"
         $_.ExportAsFixedFormat(0, $ExportPath)
     }
@@ -14,14 +14,13 @@ function Export-TervisExcelCustomerReports {
 
 function Invoke-TervisTopCustomerReportExport {
     param (
-        [Parameter(Mandatory)]$ReportDirectory,
         [Parameter(Mandatory)]$ExcelFilePath,
         [Parameter(Mandatory)]$ExportDirectory,
         [Parameter(Mandatory)]$WriteResPassword
     )
     $Excel = New-ExcelInstance
-    $Workbook = Open-ExcelFile -ExcelInstance $Excel -ExcelFilePath $ExcelFilePath -WriteResPassword $WriteResPassword -IgnoreReadOnlyRecommended $true
-    Update-ExcelFile -Workbook $Workbook
-    Export-TervisExcelCustomerReports -Workbook $Workbook -ExportDirectory $ExportDirectory
-    Stop-ExcelInstance -ExcelInstance $Excel -SaveBeforeQuit
+    $Workbook = Open-ExcelFile -ExcelInstance ([ref]$Excel) -ExcelFilePath $ExcelFilePath -WriteResPassword $WriteResPassword -IgnoreReadOnlyRecommended $true
+    Update-ExcelFile -Workbook ([ref]$Workbook)
+    Export-TervisExcelCustomerReports -Workbook ([ref]$Workbook) -ExportDirectory $ExportDirectory
+    Stop-ExcelInstance -ExcelInstance ([ref]$Excel) -SaveBeforeQuit
 }
