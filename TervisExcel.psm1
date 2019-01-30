537c336318b34fc00e05e4aae07c03057a47f787
Hamilton Perez <hambone124@users.noreply.github.com>
2019-01-30 17:47:48 -0500

    Automate Invoke-TervisTopCustomerReportExport and provision steps

--- TervisExcel.psm1
+++ TervisExcel.psm1
@@ -13,15 +13,13 @@ function Export-TervisExcelCustomerReports {
 }
 
 function Invoke-TervisTopCustomerReportExport {
-    param (
-        [Parameter(Mandatory)]$ExcelFilePath,
-        [Parameter(Mandatory)]$ExportDirectory,
-        [Parameter(Mandatory)]$WriteResPassword
-    )
+    $ReportCredential = Get-PasswordstatePassword -ID 5699
+    $ReportParameters = $ReportCredential.GenericField1 | ConvertFrom-Json
+
     $Excel = New-ExcelInstance
-    $Workbook = Open-ExcelFile -ExcelInstance ([ref]$Excel) -ExcelFilePath $ExcelFilePath -WriteResPassword $WriteResPassword -IgnoreReadOnlyRecommended $true
+    $Workbook = Open-ExcelFile -ExcelInstance ([ref]$Excel) -ExcelFilePath $ReportParameters.ExcelFilePath -WriteResPassword $ReportCredential.Password -IgnoreReadOnlyRecommended $true
     Update-ExcelFile -Workbook ([ref]$Workbook)
-    Export-TervisExcelCustomerReports -Workbook ([ref]$Workbook) -ExportDirectory $ExportDirectory
+    Export-TervisExcelCustomerReports -Workbook ([ref]$Workbook) -ExportDirectory $ReportParameters.ExportDirectory
     Stop-ExcelInstance -ExcelInstance ([ref]$Excel) -SaveBeforeQuit
 }
 
@@ -30,4 +28,9 @@ function Invoke-ExcelTaskApplicationProvision {
     $EnvironmentName = "Infrastructure"
     Invoke-ApplicationProvision -ApplicationName $ApplicationName -EnvironmentName $EnvironmentName
     $Nodes = Get-TervisApplicationNode -ApplicationName $ApplicationName -EnvironmentName $EnvironmentName
+    $Nodes | Push-TervisPowershellModulesToRemoteComputer
+    $Nodes | ForEach-Object {Invoke-Command -ComputerName $_.ComputerName -ScriptBlock {Add-LocalGroupMember -Group Administrators -Member "Privilege_InfrastructureScheduledTasksAdministrator"}}
+    $Credential = Get-PasswordstatePassword -ID 5574 -AsCredential
+    $Action = New-ScheduledTaskAction -Execute "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -Argument "-Command Invoke-TervisTopCustomerReportExport"
+    $Nodes | Install-TervisScheduledTask -TaskName "Top Customer Report Export" -Action $Action -RepetitionIntervalName "EveryDayAt730am" -Credential $Credential
 }
