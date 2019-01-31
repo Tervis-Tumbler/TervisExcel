f7760a198d986c7a78d62a66ec25f95202c8640d
Hamilton Perez <hambone124@users.noreply.github.com>
2019-01-31 17:51:29 -0500

    Add TervisExcel Polaris PowerShell Application

--- TervisExcel.psm1
+++ TervisExcel.psm1
@@ -12,7 +12,7 @@ function Export-TervisExcelCustomerReports {
     }
 }
 
-function Invoke-TervisTopCustomerReportExport {
+function Invoke-TervisTopCustomerReportExtract {
     $ReportCredential = Get-PasswordstatePassword -ID 5699
     $ReportParameters = $ReportCredential.GenericField1 | ConvertFrom-Json
 
@@ -31,6 +31,36 @@ function Invoke-ExcelTaskApplicationProvision {
     $Nodes | Push-TervisPowershellModulesToRemoteComputer
     $Nodes | ForEach-Object {Invoke-Command -ComputerName $_.ComputerName -ScriptBlock {Add-LocalGroupMember -Group Administrators -Member "Privilege_InfrastructureScheduledTasksAdministrator"}}
     $Credential = Get-PasswordstatePassword -ID 5574 -AsCredential
-    $Action = New-ScheduledTaskAction -Execute "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -Argument "-Command Invoke-TervisTopCustomerReportExport"
+    # Need to change this action to trigger the Polaris instance instead
+    $Action = New-ScheduledTaskAction -Execute "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -Argument "-Command Invoke-TervisTopCustomerReportExtract"
+    $ModuleName = "TervisTopCustomerReportExtract"
+    $Port = 9000
+    $ServiceName = "$ModuleName $Port"
     $Nodes | Install-TervisScheduledTask -TaskName "Top Customer Report Export" -Action $Action -RepetitionIntervalName "EveryDayAt730am" -Credential $Credential
+    $Nodes | Install-PowerShellApplicationPolaris -EnvironmentName Infrastructure -ModuleName $ModuleName -TervisModuleDependencies TervisExcel,ExcelPowerShell,TervisPasswordstatePowershell,PasswordstatePowershell -Ports 9000 -CommandString Start-TervisTervisTopCustomerReportPolaris
+    $Nodes | ForEach-Object { Invoke-Command -ComputerName $_.ComputerName -ScriptBlock { $C = $using:Credential; nssm set $using:ServiceName ObjectName $C.Username $C.GetNetworkCredential().Password } }
+    # $Nodes | ForEach-Object { Invoke-Command -ComputerName $_.ComputerName -ScriptBlock { $using:ModuleName; nssm get $using:ServiceName ObjectName } }
+    $Nodes | ForEach-Object { Invoke-Command -ComputerName $_.ComputerName -ScriptBlock { $using:ModuleName; nssm start $using:ServiceName } }
 }
+
+function Start-TervisTervisTopCustomerReportPolaris {
+    param (
+        [Parameter(Mandatory)]$Port
+    )
+    New-PolarisRoute -Path "/StartExtract" -Method "POST" -Scriptblock {
+        $Response.Send("Starting Top Customer Report Extract")
+        Invoke-TervisTopCustomerReportExtract
+    } -Force
+
+    New-PolarisRoute -Path "/*" -Method "GET" -Scriptblock {
+        $Response.Send("TervisExcel Polaris server is up.")
+    } -Force
+
+    $Polaris = Start-Polaris -Https -Port $Port
+
+    while ($Polaris.Listener.IsListening) {
+        Wait-Event callbackeventbridge.callbackcomplete
+    }
+}
+
+
